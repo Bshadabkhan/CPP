42172cc9c1f348fb2587c7fdbf4c15a94f523ad8
bshadabkhan <shadabkhanbenki@gmai.com>
2024-06-09 10:59:50 -0400

    Variables & datatypes

---
+++ CPPCRASH/Variables & datatypes/dtv.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //variable define
+   // int age = 20;
+    //cout << "My age is:"<< age <<endl;
+   
+   //definition
+   //int age=21;
+   //cout<<age<<endl;
+
+   //manipulation or updation
+   //age = 101;
+   //cout<<age<<endl;
+
+   //int
+   int count = 5;
+   //float
+   float share = 3.14;
+   //char
+   char alphabet = 'z';
+   //double
+   double weight = 55.56544;
+   //boolean
+   bool isMale = true;
+   bool isChild = 1;
+   bool isFemale = false;
+   bool isGirl = 0;
+
+   cout<< count << endl;
+   cout<< share<<endl;
+   cout << alphabet<<endl;
+   cout<< weight <<endl;
+   cout<< isMale<<endl;
+
+
+    return 0;
+
+}
--- Introduction/Helloworld.cpp
+++ Introduction/Helloworld.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World!";
+    int age;
+    cout << "Enter your age" << endl;
+    cin >> age;
+
+    cout << "your age is" << age << endl;
     return 0;
 }
