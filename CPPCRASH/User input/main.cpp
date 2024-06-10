97c9af9ea52fb9def1a928b2e506876b6c06abb1
bshadabkhan <shadabkhanbenki@gmai.com>
2024-06-10 11:47:26 -0400

    User input

---
+++ CPPCRASH/User input/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //variable declare
+    bool marks;
+    cout<<"Enter your marks"<<endl;
+    //input here
+    cin>>marks;
+    cout<<"your marks:"<<marks<<endl;
+
+    return 0;
+}
