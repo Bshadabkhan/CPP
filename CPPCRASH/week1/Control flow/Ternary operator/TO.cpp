51192ed39731a6989755e8a4ebe8cfe8d5ed699a
bshadabkhan <shadabkhanbenki@gmai.com>
2024-06-11 12:37:53 -0400

    Control Flow Statements

--- CPPCRASH/week1/Control flow/Ternary operator/TO.cpp
+++ CPPCRASH/week1/Control flow/Ternary operator/TO.cpp
@@ -4,13 +4,14 @@ int main(){
     // int age = 22;
     // (age>18)?cout<<"can vote":cout<<"cannot vote";
 
-    int x = 10;
-    int y = 20;
+    // int x = 10;
+    // int y = 20;
 
-    int result = (x>y)?x:y;
-    cout<<"Result:"<<result<<endl;
+    // int result = (x>y)?x:y;
+    // cout<<"Result:"<<result<<endl;
 
-
+ int age = 100;
+    (age>18)?cout<<"can vote":cout<<"cannot vote";
     
     
     
