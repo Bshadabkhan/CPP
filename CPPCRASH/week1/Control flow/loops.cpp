1471bdaa896e4800c38d39fe1c82e35a2acc27e8
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-12 13:52:49 -0400

    Loops

---
+++ CPPCRASH/week1/Control flow/loops.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //for loop
+    // for(int count = 1; count<=10; count=count+1){
+    //     cout<<"Nihal"<<endl;
+    // }
+    
+
+    // counting from 51 to 69
+    
+    // for(int i=51;i<=69;i=i+1){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=51;i<70;i=i+1){
+    //     cout<<i<<endl;
+    // }
+
+
+    //Break keyword
+
+    // for(int i =1;i<=10;i=i+1){
+    //     cout<<i<<" ";
+    //     if(i==5){
+    //         break;
+    //     }
+    // }
+
+    // for(int i =1;i<=10;i=i+1){
+    //     if(i==5){
+    //         break;
+    //     }
+    //     cout<<"Iteration "<<i<<endl;
+    // }
+
+    //continue
+
+    // for(int i=1;i<=10;i=i+1){
+    //     if(i==1)
+    //     continue;
+    //     cout<<i<<" ";
+    // }
+
+    //count 1-100;
+
+    // for(int i=1;i<=100;i=i+1){
+    //     cout<<i<<endl;
+    // }
+
+    //count 100-1;
+
+    // for(int i =100; i>=1;i=i-1){
+    //     cout<<i<<endl;
+    // }
+
+    //name 50 times
+
+    // for(int i=1;i<=50;i=i+1){
+    //     cout<<"Nihal"<<endl;
+    // }
+
+    // -0 to -10
+
+    // for(int i=0;i>=-10;i=i-1){
+    //     cout<<i<<" ";
+    // }
+
+    // table of 7
+
+    // for(int i=1;i<=10;i=i+1){
+    //     cout<<"7*"<<i<<"="<<7*i<<endl;
+    // }
+
+    // -A to Z
+
+    for(char ch='A';ch<='Z';ch=ch+1){
+        cout<<ch<<" ";
+    }
+ 
+
+    return 0;
+}
