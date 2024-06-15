a52c0c237623ad1889428cf7ebc8f11ed9296747
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-15 13:01:09 -0400

    Operators

---
+++ CPPCRASH/week1/Operators/main.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a=5;
+    // cout<<a<<endl;
+    // cout<<(++a)<<endl;
+    // //a value is 6
+    // cout<<(a++)<<endl;
+    // //if we print the value will be 6,but a value is 7 if we print a
+    // cout<<a<<endl;
+    // //a value is 7
+    // cout<<(--a)<<endl;
+    // //now a value is 6
+    // cout<<(a--)<<endl;
+    // //print will be 6 but "a" value is 5 only
+    // cout<<a<<endl;
+
+    //Arithmetic operator
+
+    // int a=10;
+    // int b=5;
+
+    // cout<<(a+b)<<endl;
+    // cout<<(a-b)<<endl;
+    // cout<<(a*b)<<endl;
+    // cout<<(a/b)<<endl;
+    // cout<<(a%b)<<endl;
+
+    // cout<<(5*1.0/2)<<endl;
+
+    // cout<<(10>5)<<endl;
+    // cout<<(10<5)<<endl;
+    // cout<<(10>=5)<<endl;
+    // cout<<(10<=5)<<endl;
+    // cout<<(10==5)<<endl;
+    // cout<<(10!=5)<<endl;
+    
+    //and operator
+
+
+    // bool cond1 = true;
+    // bool cond2 = true;
+    // bool cond3 = false;
+    // if(cond1 && cond2 && cond3){
+    //     cout<<"all conditions are true"<<endl;
+    // }
+    // else{
+    //     cout<<"all conditions are false";
+    // }
+
+    //Or operator
+
+    // bool cond1 = (1>=5);
+    // bool cond2 = (5>=10);
+    // bool cond3 = (2!=2);
+    // if(cond1 || cond2 || cond3){
+    //     cout<<"atleast one conditions are true"<<endl;
+    // }
+    // else{
+    //     cout<<"all conditions are false";
+    // }
+
+    //Logical not
+
+    // bool condition = (5 != 10);
+    // cout<<!condition<<endl;
+
+    // bool condition = (5 != 10);
+    // cout<<condition<<endl;
+
+    //Assignment operator
+    
+    
+    // int arr=5;
+    // cout<<arr<<endl;
+
+    // arr+=10;
+    // cout<<arr<<endl;
+
+    // Bitwise Operator
+
+    //bitwise operator
+    
+    cout<<(5&4)<<endl;//bitwise and op
+    cout<<(5|4)<<endl;//bitwise or op
+    cout<<(~1)<<endl;//bitwise tilda operator
+    cout<<(5<<4)<<endl;//left shift operator
+    cout<<(100>>3)<<endl;//right shift operator
+    cout<<(5^5)<<endl;//bitwise XOR op
+    cout<<(5^4)<<endl;//bitwise XOR op
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
