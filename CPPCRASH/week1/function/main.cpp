72ca19bd4a642a6e4bd8ce898a339b3e574f6b8e
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-20 13:21:32 -0400

    functions

--- CPPCRASH/week1/function/main.cpp
+++ CPPCRASH/week1/function/main.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+void printCounting(){
     for(int i=1;i<=5;i++){
-        cout<<i<<endl;
-    }
+        cout<<i<<" ";
+    }cout<<endl;
+
+}
+
+int main(){
+    
+    printCounting();
+    cout<<"khan"<<endl;
+    
+   
+    int a;
+    cin>>a;
+    cout<<a<<endl;   
+
+   printCounting();
+
+
+
     return 0;
 }
---
+++ CPPCRASH/week1/function/sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+    int totalSum=a+b;
+    return totalSum;
+}
+
+int main(){
+
+    int ans = sum(5,10);
+    cout<<ans<<endl;
+    
+
+
+
+
+
+
+
+    return 0;
+}
