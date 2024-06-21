7586f7cb007dbb4d95a5edc3849bb5f0f6e50de9
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-21 10:42:55 -0400

    functions

--- CPPCRASH/week1/function/sum.cpp
+++ CPPCRASH/week1/function/sum.cpp
@@ -5,13 +5,16 @@ int sum(int a,int b){
     int totalSum=a+b;
     return totalSum;
 }
-
+void printMyName(){
+    cout<<"Khan"<<endl;
+}
 int main(){
 
-    int ans = sum(5,10);
-    cout<<ans<<endl;
+    // int ans = sum(5,10);
+    // cout<<ans <<endl;
     
-
+    //function call
+    printMyName();
 
 
 
