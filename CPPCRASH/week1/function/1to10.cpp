c26df3f09d9b30d4dee0799ad433e6e96574d317
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-21 11:22:45 -0400

    1to100

---
+++ CPPCRASH/week1/function/1to10.cpp
@@ -0,0 +1,22 @@
+//Write a function to print counting from 1 to 100\
+
+#include<iostream>
+using namespace std;
+
+int counting(){
+    for(int i = 1; i<=100;i++){
+        cout<<i<<endl;
+    }
+}
+
+
+
+int main(){
+
+    counting();
+
+
+
+
+    return 0;
+}
