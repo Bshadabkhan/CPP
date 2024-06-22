db680f2172f90186de0852389dcf39f9a1a10f9c
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-22 10:47:38 -0400

    traverse

---
+++ CPPCRASH/week1/Array/arr.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int multipleof2[10] = {2,4,6,8,10,12,14,16,18,20};
+    cout<<multipleof2[0]<<endl;
+    cout<<multipleof2[1]<<endl;
+    cout<<multipleof2[2]<<endl;
+    cout<<multipleof2[9]<<endl;
+
+    //traverse
+    // for(int i=0;i<=9;i++){
+    //     cout<<multipleof2[i]<<" ";
+    // }
+
+
+    return 0;
+}
