00cd98a55cbb9ad000c7151b4d468f200ec889f6
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-22 10:33:28 -0400

    array

---
+++ CPPCRASH/week1/Array/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //declaration
+
+    int man[101];
+    int arr[10];
+
+    //initialization
+    int brr[5]={10,20,30,40,50};
+    int crr[]={10,20,30,40};
+
+    return 0;
+}
