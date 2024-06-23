b1ca463ab8e68a4d98c32d77597795f73343c4f6
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-23 12:30:10 -0400

    array functions

--- CPPCRASH/week1/Array/2dfunction.cpp
+++ CPPCRASH/week1/Array/2dfunction.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
-
+ 
 void print2DArray(int arr[][4],int rowSize, int colSize){
-
+    cout<<"Printing the 2D array:"<<endl;
+    //printing
+    for(int row_index=0; row_index<rowSize;row_index++){
+        for(int col_index=0;col_index<colSize;col_index++){
+            cout<<arr[row_index][col_index]<<" ";
+        }
+        cout<<endl;   
+    }
 }
 
 int main(){
@@ -19,14 +26,7 @@ int main(){
         cout<<endl;   
     }
 
-    // //printing
-    for(int row_index=0; row_index<=row-1;row_index++){
-        for(int col_index=0;col_index<=col-1;col_index++){
-            cout<<arr[row_index][col_index]<<" ";
-        }
-        cout<<endl;   
-    }
-
+   
     print2DArray(arr,row,col);
 
 
