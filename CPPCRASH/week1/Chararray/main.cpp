3ebaf0b065a08e25d219c88fd4083886e4580f7b
bshadabkhan <shadabkhanbenki@gmail.com>
2024-06-26 11:25:48 -0400

    character array

---
+++ CPPCRASH/week1/Chararray/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //declare
+    //char arr[10];
+   // cout<<arr[3]<<endl;
+
+//    char arr[10] = "khan";
+//    cout<<arr[0]<<endl;
+//    cout<<arr[1]<<endl;
+//    cout<<arr[2]<<endl;
+//    cout<<arr[3]<<endl;
+
+//    cout<<"printing 4th index:"<<arr[4]<<endl;//null character
+//    int asciiValue = arr[4];
+//    cout<<asciiValue<<endl;
+
+char arr[4];
+arr[0] = 'a';
+arr[1]='b';
+arr[2]='\0';
+
+
+    return 0;
+}
