6e510eb6a14f42d2c087fa4a524f0e5e88e42405
bshadabkhan <shadabkhanbenki@gmail.com>
2024-07-02 19:35:58 +0530

    Strings

---
+++ CPPCRASH/week1/Strings/memory.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "Example";
+    // std::string automatically handles memory allocation and deallocation.
+    return 0;
+}
+
+
